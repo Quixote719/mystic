0b7d73383a92d713cce86e8ba29fbac077a3a27d
Liang Wang <wangliang14@hkvision.com>
2018-05-22 16:56:35 +0800

    'hotmodule'

--- src/app.js
+++ src/app.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css';
 import './app.scss';
+import { Table } from 'antd';
 
+
+var data = [];
+
+for (let i=0; i<400; i++) {
+    data.push({
+        key: i,
+        name: 'IronMan' + i,
+        age: 32,
+        address: '北海区紫陌路' + i + '号'
+    });
+}
+
+const columns = [{
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+}, {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+}, {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+}];
 
 class App extends Component {
     render() {
@@ -8,9 +35,10 @@ class App extends Component {
             <div className="App">
                 <div className="block"></div>
                 <p className="App-intro">
-                    I dream and the world trembles
+                  I dream and the world trembles.
                 </p>
                 <div className="deck"></div>
+                <Table dataSource={data} columns={columns} />
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'build')
+    },
+    devServer: {
+      contentBase: "./",
+      compress: true,
+      port: 9000,
+      hot: true,
+      open: true,
     },
     resolve: {
         extensions: ['.js', '.jsx']
@@ -32,5 +41,9 @@ module.exports = {
                 use: ['style-loader','css-loader']
             }
         ]
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({template: './index.html'}),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
