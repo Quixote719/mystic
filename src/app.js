e7c065b28c07860efe4e829a0c46b3ec8274ba31
Liang Wang <wangliang14@hkvision.com>
2018-05-25 14:43:51 +0800

    little upgrade

--- src/app.js
+++ src/app.js
@@ -1,33 +1,6 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css';
 import './app.scss';
-import { Table } from 'antd';
 
-
-var data = [];
-
-for (let i=0; i<400; i++) {
-    data.push({
-        key: i,
-        name: 'IronMan' + i,
-        age: 32,
-        address: '北海区紫陌路' + i + '号'
-    });
-}
-
-const columns = [{
-    title: '姓名',
-    dataIndex: 'name',
-    key: 'name',
-}, {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-}, {
-    title: '住址',
-    dataIndex: 'address',
-    key: 'address',
-}];
 
 class App extends Component {
     render() {
@@ -38,7 +11,6 @@ class App extends Component {
                   I dream and the world trembles.
                 </p>
                 <div className="deck"></div>
-                <Table dataSource={data} columns={columns} />
             </div>
         );
     }
