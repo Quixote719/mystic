898ba2c6a39964b137e8fc22951921bb7fb0d62f
王良14 <wangliang14@hikvision.com>
2018-11-29 18:49:05 +0800

    check prettier

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-import './app.scss';
-
+import React, { Component } from "react";
+import "./app.scss";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="block"></div>
-                <p className="App-intro">
-                  I dream and the world trembles.
-                </p>
-                <div className="deck"></div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <div className="block" />
+        <p className="App-intro">I dream and the world trembles</p>
+        <div className="deck" />
+      </div>
+    );
+  }
 }
 
 export default App;
