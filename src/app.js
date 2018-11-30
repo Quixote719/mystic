56b775fb8b7738c18688f0c98a309f9aa70fa2bf
王良14 <wangliang14@hikvision.com>
2018-11-30 12:53:37 +0800

    eslint is working

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './app.scss'
 
-class App extends Component {
+class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <div className="block" />
-
                 <p className="App-intro">I dream and the world trembles</p>
+                <p className="App-intro">Halelujah</p>
                 <div className="deck" />
             </div>
         )
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin') //installed via npm
 
 module.exports = {
     entry: './src/index.js',
@@ -9,11 +9,11 @@ module.exports = {
         path: path.resolve(__dirname, 'build')
     },
     devServer: {
-      contentBase: "./",
-      compress: true,
-      port: 9000,
-      hot: true,
-      open: true,
+        contentBase: './',
+        compress: true,
+        port: 9000,
+        hot: true,
+        open: true
     },
     resolve: {
         extensions: ['.js', '.jsx']
@@ -21,29 +21,29 @@ module.exports = {
     module: {
         rules: [
             {
-                test:/\.(js|jsx)$/,
-                exclude:/node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: [
-                        "react",
-                        "es2015",
-                        "stage-2"
-                    ]
-                }
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+                // query: {
+                //     presets: [
+                //         "react",
+                //         "es2015",
+                //         "stage-2"
+                //     ]
+                // }
             },
             {
-                test:/\.(sass|scss)$/,
-                use:['style-loader','css-loader','sass-loader']
+                test: /\.(sass|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             },
             {
                 test: /\.(css)$/,
-                use: ['style-loader','css-loader']
+                use: ['style-loader', 'css-loader']
             }
         ]
     },
-    plugins:[
-        new HtmlWebpackPlugin({template: './index.html'}),
+    plugins: [
+        new HtmlWebpackPlugin({ template: './index.html' }),
         new webpack.HotModuleReplacementPlugin()
     ]
-};
+}
