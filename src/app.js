b53cd7098e79074f85c6a585679a6981d29321ab
王良14 <wangliang14@hikvision.com>
2018-12-03 17:43:21 +0800

    eslint-plugin-prettier

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@ import './app.less'
 class App extends React.Component {
     render() {
         return (
-            <div className="App">
-                <div className="block" />
-                <p className="App-intro">
+            <div className='App'>
+                <div className='block' />
+
+                <p className='App-intro'>
                     In silience and hope will be your strength
                 </p>
-                <p className="App-intro">
-                    Right now three things remain: faith, hope, and love.
+                <p className='App-intro'>
+                    Right now three things remain: faith, hope, and love
                 </p>
 
-                <div className="deck" />
+                <div className='deck' />
             </div>
         )
     }
