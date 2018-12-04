473fec09386c19113a3c2b896544bcca735f17a6
LAPTOP-URN1R8TL\lwang <dwang@dsny.nyc.gov>
2018-12-04 11:39:11 +0800

    禁用规则示例

--- src/app.js
+++ src/app.js
@@ -3,17 +3,21 @@ import './app.less'
 
 class App extends React.Component {
     render() {
+        /* eslint-disable no-alert, no-console */
+        alert(7)
+        console.log(33)
+        /* eslint-enable no-alert, no-console */
+        // eslint-disable-next-line
+        alert('foo');
         return (
             <div className='App'>
                 <div className='block' />
-
                 <p className='App-intro'>
                     In silience and hope will be your strength
                 </p>
                 <p className='App-intro'>
                     Right now three things remain: faith, hope, and love
                 </p>
-
                 <div className='deck' />
             </div>
         )
