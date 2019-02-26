81dffcd5cfafaac1ee7c22f5c2fa7cfeb7f56a98
王良14 <wangliang14@hikvision.com>
2019-02-26 15:47:23 +0800

    change eslint and prettier config file type

--- src/pages/Jupiter.jsx
+++ src/pages/Jupiter.jsx
@@ -3,6 +3,7 @@ import './app.less'
 
 class App extends React.Component {
     render() {
+        console.warn('this is Jupiter')
         return (
             <div className='App'>
                 <p className='App-intro'>welcome to Jupiter</p>
