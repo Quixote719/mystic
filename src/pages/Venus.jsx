e93ec8252e675989d93c2ca456f82afb9ae83521
王良14 <wangliang14@hikvision.com>
2019-04-17 16:57:08 +0800

    plugin for rest spread

--- src/pages/Venus.jsx
+++ src/pages/Venus.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import styles from './app.less'
 
 class App extends React.Component {
-    formValidation = () => {}
+    constructor(props) {
+        super(props)
+        this.state = {
+            venus: 2
+        }
+    }
+    formValidation = () => {
+        let a = [1, 2, 3]
+        let b = [7, 8, 9]
+        const c = [...a, ...b]
+        return c
+    }
     render() {
+        console.warn(this.formValidation())
         return (
             <div className={styles.App}>
                 <p className={styles.appIntro}>welcome to Venus</p>
