26e45e61ec4e70eb9851664a1e694becd71a9ca4
王良14 <wangliang14@hikvision.com>
2019-04-17 13:41:54 +0800

    add transform-class-properties to allow arrow func in react

--- src/pages/Venus.jsx
+++ src/pages/Venus.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './app.less'
 
 class App extends React.Component {
+    formValidation = () => {}
     render() {
         return (
             <div className={styles.App}>
