32a20a66c409208e90edaecfae46d5a55bbaf65f
Quixote719 <wangliang14@hkvision.com>
2019-07-30 16:51:20 +0800

    Knowledge Graph Initialization

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './pages/home'
-import Mercury from './pages/Mercury'
-import Venus from './pages/Venus'
 import Earth from './pages/Earth'
-import Mars from './pages/Mars'
-import Jupiter from './pages/Jupiter'
-import Saturn from './pages/Saturn'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import promise from 'redux-promise'
@@ -25,14 +19,8 @@ class App extends React.PureComponent {
             <Provider store={createStore(reducers, middleware)}>
                 <Router>
                     <div>
-                        <Home />
                         <Switch>
-                            <Route path='/Mercury' component={Mercury} />
-                            <Route path='/Venus' component={Venus} />
                             <Route path='/Earth' component={Earth} />
-                            <Route path='/Mars' component={Mars} />
-                            <Route path='/Jupiter' component={Jupiter} />
-                            <Route path='/Saturn' component={Saturn} />
                         </Switch>
                     </div>
                 </Router>
--- src/pages/Earth.jsx
+++ src/pages/Earth.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 // import * as algorithm from '../algorithm/index'
+import { Steps, Icon } from 'antd'
 import styles from './app.less'
+
+const { Step } = Steps
 
 class App extends React.Component {
     render() {
         return (
             <div className={styles.App}>
                 <p className={styles.appIntro}>welcome to Earth</p>
+                <Steps style={{ width: 1400, margin: 'auto' }}>
+                    <Step
+                        status='finish'
+                        title='Login'
+                        icon={<Icon type='user' />}
+                    />
+                    <Step
+                        status='finish'
+                        title='Verification'
+                        icon={<Icon type='solution' />}
+                    />
+                    <Step
+                        status='finish'
+                        title='Pay'
+                        icon={<Icon type='loading' />}
+                    />
+                    <Step
+                        status='wait'
+                        title='Done'
+                        icon={<Icon type='smile-o' />}
+                    />
+                </Steps>
             </div>
         )
     }
